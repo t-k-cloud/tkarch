0bddac7cc7130434c6fc49dac8b38424a4c523ce
t-k- <clock126@126.com>
2015-10-05 15:25:37 -0400

    replace chinese font to Monospaced wqy

--- my-arch-post-install.sh
+++ my-arch-post-install.sh
@@ -29,7 +29,7 @@ ln -sf `which vim` `which vi`
 tput setaf 2; echo 'Installing desktop environment...'; tput sgr0;
 pacman --noconfirm -S xorg-server xorg-xinit xterm # X server
 pacman --noconfirm -S xf86-input-synaptics # touchpad/touchscreen
-pacman --noconfirm -S adobe-source-han-sans-cn-fonts # chinese font
+pacman --noconfirm -S wqy-microhei # chinese font, Monospaced font included
 pacman --noconfirm -S ttf-liberation # TTF font (otherwise MathJax is slow)
 pacman --noconfirm -S cinnamon # desktop environment
 
