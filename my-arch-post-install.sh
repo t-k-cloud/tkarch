361f3a6ab1e0131195c8c7e27d5cade2d85c4588
t-k- <clock126@126.com>
2016-04-08 19:43:24 -0600

    'update'

--- my-arch-post-install.sh
+++ my-arch-post-install.sh
@@ -137,7 +137,7 @@ cat << EOF > /usr/share/applications/my-terminal.desktop
 Name=My Terminal
 Comment=Use the command line
 Keywords=shell;prompt;command;commandline;
-Exec=mate-terminal -e "bash -c 'cd ~/Desktop;tmux;exec bash'"
+Exec=mate-terminal -e "bash -c 'cd ~/Desktop;tmux -2;exec bash'"
 Icon=utilities-terminal
 Type=Application
 Categories=GNOME;GTK;System;TerminalEmulator;
