787d8afd242b253afe10df31dcc78ccac3eddaa5
t-k- <clock126@126.com>
2016-08-01 06:00:27 -0400

    avoid deprecated php functions

--- test-php/test-db.php
+++ test-php/test-db.php
@@ -4,17 +4,19 @@ $password = "xxxxxxxxxxxxx";
 $hostname = "localhost"; 
 
 //connection to the database
-$db = mysql_connect($hostname, $username, $password) 
+$db = mysqli_connect($hostname, $username, $password) 
   or die("Unable to connect to MySQL");
 
 echo "Connected to MySQL<br>";
 
-$result = mysql_query("select * from information_schema.tables ".
-                      "group by engine");
+$result = mysqli_query($db,
+	"select * from information_schema.tables ".
+	"group by engine"
+);
 
-while ($row = mysql_fetch_array($result)) {
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
       echo $row{'TABLE_NAME'}."'s engine is ".$row{'ENGINE'}."<br>";
 }
 
-mysql_close($db);
+mysqli_close($db);
 ?>
