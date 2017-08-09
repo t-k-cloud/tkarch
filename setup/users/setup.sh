4aeb232fe8acf5685f0c2826abbcb7c20bb76bed
t-k- <clock126@126.com>
2017-08-09 12:16:12 +0800

    Enable specify password in config file.

--- setup/users/setup.sh
+++ setup/users/setup.sh
@@ -1,9 +1,17 @@
 # setup for root
-echo 'Please create root password:'
-while ! passwd; do :; done
+if [ -z $ROOTPASS ]; then
+	echo 'Please set a root password:'
+	while ! passwd; do :; done
+else
+	echo "$ROOTPASS" | passwd --stdin
+fi
 
 # setup for user
 useradd -m -G wheel -s /bin/bash $USERNAME
 
-echo "Please create $USERNAME password:"
-while ! passwd $USERNAME; do :; done
+if [ -z $USERPASS ]; then
+	echo "Please set $USERNAME password:"
+	while ! passwd $USERNAME; do :; done
+else
+	echo "$USERPASS" | passwd --stdin $USERNAME
+fi
