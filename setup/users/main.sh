dfc9d64510ed5a4dd639e63e62bf520545f44205
t-k- <clock126@126.com>
2017-08-09 11:04:09 +0800

    prompt text added

--- setup/users/main.sh
+++ setup/users/main.sh
@@ -1,6 +1,9 @@
 # setup for root
+echo 'Please create root password:'
 while ! passwd; do :; done
 
 # setup for user
 useradd -m -G wheel -s /bin/bash $USERNAME
+
+echo "Please create $USERNAME password:"
 while ! passwd $USERNAME; do :; done
