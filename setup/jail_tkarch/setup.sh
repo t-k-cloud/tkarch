0c047d5df400a73e6ae193e08e6460cf30bbc674
t-k- <clock126@126.com>
2017-10-07 02:40:55 -0400

    it is good to always have .git

--- setup/jail_tkarch/setup.sh
+++ setup/jail_tkarch/setup.sh
@@ -1,2 +1,3 @@
 rm -rf /mnt/root/tkarch
-rsync -a --exclude='.git/' "$TKARCH_DIR/" /mnt/root/tkarch
+# rsync -a --exclude='.git/' "$TKARCH_DIR/" /mnt/root/tkarch
+rsync -a "$TKARCH_DIR/" /mnt/root/tkarch
