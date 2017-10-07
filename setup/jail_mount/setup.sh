b36ce88c9702cb945d7a125edc1a95b9a5676849
t-k- <clock126@126.com>
2017-10-07 01:52:25 -0400

    fixes.

--- setup/jail_mount/setup.sh
+++ setup/jail_mount/setup.sh
@@ -1,6 +1,6 @@
-mount $DISKPART_ROOTDEV /mnt
+mount $DISKPART_ROOT /mnt
 mkdir -p /mnt/boot
-mount $DISKPART_BOOTDEV /mnt/boot
+mount $DISKPART_BOOT /mnt/boot
 
 if $GRUB_ELF_INSTALL; then
 	mkdir -p /mnt/boot/efi
