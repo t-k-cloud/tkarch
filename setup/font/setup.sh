9b3fe6b642b323a74b55a360437a08a9cf2b3deb
t-k- <clock126@126.com>
2018-02-25 23:39:15 -0500

    install inconsolate

--- setup/font/setup.sh
+++ setup/font/setup.sh
@@ -1,6 +1,7 @@
 pacmanS wqy-microhei # chinese font, Monospaced font included
 pacmanS ttf-liberation # TTF font (otherwise MathJax is slow)
 pacmanS ttf-droid # to be used as cinnamon default font to avoid Chromium address bar delay.
+pacmanS community/ttf-inconsolata # monospaced font for programming
 
 
 cat << EOF >> /home/$USERNAME/.xinitrc
