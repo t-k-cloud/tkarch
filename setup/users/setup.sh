0a2bde887883b9116bef9de2da2a9f1340ca19d9
t-k- <clock126@126.com>
2018-08-12 19:14:42 -0400

    remove ROOTPASS and USERPASS

--- setup/users/setup.sh
+++ setup/users/setup.sh
@@ -1,17 +1,1 @@
-# setup for root
-if [ -z $ROOTPASS ]; then
-	echo 'Please set a root password:'
-	while ! passwd; do :; done
-else
-	: # passwd --stdin deprecated, use chpasswd.
-fi
-
-# setup for user
 useradd -m -G wheel -s /bin/bash $USERNAME
-
-if [ -z $USERPASS ]; then
-	echo "Please set $USERNAME password:"
-	while ! passwd $USERNAME; do :; done
-else
-	: # passwd --stdin deprecated, use chpasswd.
-fi
