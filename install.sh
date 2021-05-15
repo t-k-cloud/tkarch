fc56343e7e3a7a3a09f835f32fe7e92b29fe741f
t-k- <clock126@126.com>
2021-05-15 06:37:56 -0400

    install wget as the new archiso does not have that!

--- install.sh
+++ install.sh
@@ -2,19 +2,24 @@
 TKARCH_DIR=$(cd `dirname $0`; pwd)
 source ${TKARCH_DIR}/lib/common.sh
 
-### install ###
+### Prompt user to modify mirrorlist ###
 setup mirrorlist
 
+### Ensure we have Internet access ###
 if $DO_CONN; then
 	setup wifiwpa
 fi;
 
+pacman -Sy
+which wget | pacmanS wget
 internet || exit 
 
+### Do dist partition ###
 if $DO_PART; then
 	setup diskpart
 fi;
 
+### Prepare jail environment ###
 setup jail_mount
 
 if $DO_JAIL_SETUP; then
@@ -24,5 +29,7 @@ fi;
 
 pacmanS rsync
 setup jail_tkarch
+
+### Change root to jail and do post-install
 setup arch_chroot
 echo 'Now remove USB drive and reboot.'
