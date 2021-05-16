ea98308ad17f7a85dd15c1f2ad5e398826c4a51b
t-k- <clock126@126.com>
2021-05-16 02:01:07 -0400

    fixup and update.

--- setup/pacman_extra/setup.sh
+++ setup/pacman_extra/setup.sh
@@ -1,12 +1,13 @@
 pacmanS \
 	wget curl enca ctags expect dnsutils mlocate dosfstools parted \
-	fuse-exfat exfat-utils python-pip mplayer htop imagemagick
+	fuse-exfat exfat-utils python-pip mplayer htop imagemagick nodejs npm
 
 pacmanS evince # pdf reader that supports .djvu format
 pacmanS xournal # pdf annotation/note
 
 # install proxy tools
-pacmanS shadowsocks-libev polipo
+pacmanS shadowsocks-libev simple-obfs polipo
+ln -sf /usr/bin/obfs-local /usr/local/bin/obfs-local
 cat << EOF > /etc/polipo/config
 socksParentProxy = "localhost:3080"
 socksProxyType = socks5
