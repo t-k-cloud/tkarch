86628f201bf01b66792d1c6ae7a9ddbceb2021a5
t-k- <clock126@126.com>
2022-05-15 03:28:08 -0400

    font and .gitconfig update

--- setup/font/setup.sh
+++ setup/font/setup.sh
@@ -1,4 +1,5 @@
-pacmanS wqy-microhei # chinese font, Monospaced font included
+pacmanS wqy-microhei # Chinese font, Monospaced font included
+pacmanS poppler-data # essential Chinese fonts for some PDFs
 pacmanS ttf-liberation # TTF font (otherwise MathJax is slow)
 pacmanS ttf-droid # to be used as cinnamon default font to avoid Chromium address bar delay.
 pacmanS community/ttf-inconsolata # monospaced font for programming
