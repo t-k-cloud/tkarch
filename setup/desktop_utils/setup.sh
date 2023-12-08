ea42ecc825f1d4a2ab56e7dfef58598ae1b4927e
w32zhong <clock126@126.com>
2023-12-07 23:49:12 -0500

    install GUI commands in the right place!

--- setup/desktop_utils/setup.sh
+++ setup/desktop_utils/setup.sh
@@ -9,3 +9,7 @@ pacmanS file-roller # archive manager
 pacmanS gnome-keyring # see below
 # If applet is not prompting for a password when connecting to new wifi networks, and is just disconnecting immediately, you may need to install gnome-keyring.
 pacmanS xfce4-clipman-plugin # clipman
+pacmanS imagemagick # simple image editor
+pacmanS mplayer # powerful media player
+pacmanS zathura-pdf-mupdf # using fast pdf engine and VI-like, supports history.
+pacmanS xournal # pdf annotation/note
--- setup/pacman_extra/setup.sh
+++ setup/pacman_extra/setup.sh
@@ -1,7 +1,5 @@
+# here we only install non-GUI packages, install GUI commands in desktop_utils!
 pacmanS \
 	wget curl enca ctags expect dnsutils mlocate dosfstools parted \
-	fuse-exfat exfat-utils python-pip mplayer htop imagemagick nodejs npm \
+	fuse-exfat exfat-utils python-pip htop nodejs npm \
 	man-db man-pages
-
-pacmanS zathura-pdf-mupdf # using fast pdf engine and VI-like, supports history.
-pacmanS xournal # pdf annotation/note
