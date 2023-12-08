493cb3a563184db275bbd0299e358ef1103c180d
w32zhong <clock126@126.com>
2023-12-07 23:40:44 -0500

    mv tk-translator.sh to global_scripts.

--- setup/translator/setup.sh
+++ setup/translator/setup.sh
@@ -1,5 +1,4 @@
 pacmanS xclip translate-shell
-
 # xclip file_name              # selection copy file content
 # xclip -selection c file_name # Ctrl+C file content
 # xclip -o                     # output selection copy
@@ -9,5 +8,3 @@ pacmanS xclip translate-shell
 # trans en:zh -b [Eel]         # brief output, using Bing engine
 # trans -identify 中文         # detect language
 # trans -speak 鳗鱼炒饭        # TTS
-
-cp tk-translator.sh /usr/local/bin/tk-translator.sh
