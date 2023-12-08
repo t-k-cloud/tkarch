9e56baa0ca4e65a0f707e74344fbc419f11420fd
Wei <clock126@126.com>
2023-12-08 14:03:49 -0500

    Update setup.sh

--- setup/gschema/setup.sh
+++ setup/gschema/setup.sh
@@ -1,3 +1,7 @@
+# Install wallpaper for /usr/share/backgrounds/archlinux/simple.png
+pacmanS archlinux-wallpaper
+
+# generate compiled schemas for dconf on entering desktop env
 cp ./10_tkarch.gschema.override /usr/share/glib-2.0/schemas/
 glib-compile-schemas /usr/share/glib-2.0/schemas/
 rm -f /home/${USERNAME}/.config/dconf/user
