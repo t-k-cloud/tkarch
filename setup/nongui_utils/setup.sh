010fb2b98610382e78d33c6f2d40606ff3856a52
Wei <clock126@126.com>
2023-12-08 17:37:43 -0500

    Update setup.sh

--- setup/nongui_utils/setup.sh
+++ setup/nongui_utils/setup.sh
@@ -1,5 +1,5 @@
 # here we only install non-GUI packages, install GUI commands in desktop_utils!
 pacmanS \
 	wget curl enca ctags expect dnsutils mlocate dosfstools parted \
-	fuse-exfat exfat-utils python-pip htop nodejs npm \
+	hwinfo fuse-exfat exfat-utils python-pip htop nodejs npm \
 	man-db man-pages
