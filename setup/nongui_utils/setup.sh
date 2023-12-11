2d91f89e743a92d2a42d7aaee804b476b3111952
w32zhong <clock126@126.com>
2023-12-10 19:05:58 -0500

    update nongui_utils

--- setup/nongui_utils/setup.sh
+++ setup/nongui_utils/setup.sh
@@ -1,5 +1,5 @@
 # here we only install non-GUI packages, install GUI commands in desktop_utils!
 pacmanS \
 	wget curl enca ctags expect dnsutils mlocate dosfstools parted \
-	hwinfo fuse-exfat exfat-utils python-pip htop nodejs npm \
-	man-db man-pages
+	hwinfo fuse-exfat exfat-utils python-pip htop nodejs npm nfs-utils \
+	man-db man-pages ncdu
