47a90b788246b4b3f3e2ab6f26c2c9a291dc57f3
w32zhong <clock126@126.com>
2023-12-12 18:46:42 -0500

    added SERVICES entry in config.

--- setup/systemd_services/setup.sh
+++ setup/systemd_services/setup.sh
@@ -1,3 +1,9 @@
 # let symbol links be from /home tkarch instead of from /root tkarch!
 /home/${USERNAME}/tkarch/systemd_services/init.sh /etc/systemd/system
-chmod 664 /etc/systemd/system/tk-*.service
+
+chmod 664 /etc/systemd/system/*.service
+
+for srv in $SERVICES; do
+    echo "Enabling systemd service: $srv";
+    systemctl enable $srv
+done
