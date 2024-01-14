30b561f0c88fa737cc42b8b5bed50dd9d40059c5
w32zhong <clock126@126.com>
2024-01-14 13:01:48 -0500

    enable docker and added /etc/docker/daemon.json

--- setup/nongui_utils/setup.sh
+++ setup/nongui_utils/setup.sh
@@ -2,4 +2,25 @@
 pacmanS \
 	wget curl enca ctags expect dnsutils mlocate dosfstools parted \
 	hwinfo fuse-exfat exfat-utils python-pip htop nodejs npm nfs-utils \
-	man-db man-pages ncdu docker
+	man-db man-pages ncdu
+
+# install docker
+pacmanS docker
+DOCKER_MIRROR=https://docker.io
+mkdir -p /etc/docker
+cat > '/etc/docker/daemon.json' << EOF
+{
+	"registry-mirrors": ["$DOCKER_MIRROR"],
+
+	"log-driver": "json-file",
+	"log-opts": {
+		"max-size": "10m",
+		"max-file": "3"
+	},
+
+	"metrics-addr" : "0.0.0.0:9323",
+	"experimental" : true
+}
+EOF
+# To get metrics: `curl 127.0.0.1:9323/metrics`
+systemctl enable docker
