a9fa879a98b596b1d17b7dc387aae2410b567eb0
w32zhong <clock126@126.com>
2024-04-17 20:26:05 -0400

    update tk-cluster-bkup.sh

--- global_scripts/tk-cluster-bkup.sh
+++ global_scripts/tk-cluster-bkup.sh
@@ -3,3 +3,6 @@ year=$(date +%Y)
 rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/$year rsync://truenas:30026/rsync_root/master-tree/incr/
 rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/install rsync://truenas:30026/rsync_root/master-tree/incr/
 rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/materials rsync://truenas:30026/rsync_root/master-tree/incr/
+
+rsync --progress -zauv --delete --exclude='.git/' rsync://truenas:30026/rsync_root/master-tree/proj/hippo ~/master-tree/proj/
+rsync --progress -zauv --delete --exclude='.git/' rsync://truenas:30026/rsync_root/master-tree/proj/tkblog ~/master-tree/proj/
