c1ec47ce002be332692adb17432244845f176533
w32zhong <clock126@126.com>
2024-07-03 23:30:09 -0400

    remove incr/install backup.

--- global_scripts/tk-cluster-bkup.sh
+++ global_scripts/tk-cluster-bkup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 year=$(date +%Y)
 rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/$year rsync://truenas:30026/rsync_root/master-tree/incr/
-rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/install rsync://truenas:30026/rsync_root/master-tree/incr/
 rsync --progress -zauv --delete --exclude='.git/' ~/master-tree/incr/materials rsync://truenas:30026/rsync_root/master-tree/incr/
 
 rsync --progress -zauv --delete --exclude='.git/' rsync://truenas:30026/rsync_root/master-tree/proj/hippo ~/master-tree/proj/
