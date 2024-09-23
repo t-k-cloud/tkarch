ba48d4f950ce957cf0832cef12468ba739a3644b
w32zhong <clock126@126.com>
2024-09-23 13:06:58 -0400

    update setup/font/setup.sh

--- setup/font/setup.sh
+++ setup/font/setup.sh
@@ -3,6 +3,7 @@ pacmanS poppler-data # essential Chinese fonts for some PDFs
 pacmanS ttf-liberation # TTF font (otherwise MathJax is slow)
 pacmanS ttf-droid # to be used as cinnamon default font to avoid Chromium address bar delay.
 pacmanS ttf-inconsolata # monospaced font for programming
+pacmanS libertinus-font # for math font
 pacmanS noto-fonts-emoji
 
 cat << EOF >> /home/$USERNAME/.xinitrc
