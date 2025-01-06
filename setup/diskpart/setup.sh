5d8a67530ac88430e293df0fe7bf95b9e5fe8fde
w32zhong <clock126@126.com>
2025-01-06 17:46:38 -0500

    rewrite disk-part and grub-install for UEFI install.

--- setup/diskpart/setup.sh
+++ setup/diskpart/setup.sh
@@ -10,43 +10,36 @@ N_ROOT=$(echo_dev_num $DISKPART_ROOT)
 
 parted $DISKPART_DEV --script print
 
-if ! $GRUB_ELF_INSTALL; then
-	# erase partition table completely
-	sectors=`blockdev --getsize $DISKPART_DEV`
-	tput setaf 2; echo "$DISKPART_DEV has $sectors sectors in total, erase in 10 sec..."; tput sgr0;
-	sleep 10
-	tput setaf 2; echo "erase disk head ..."; tput sgr0;
-	dd if=/dev/zero of=$DISKPART_DEV bs=512 count=8192
-	tput setaf 2; echo "erase disk tail ..."; tput sgr0;
-	dd if=/dev/zero of=$DISKPART_DEV bs=512 seek=`expr $sectors - 8192` count=8192
+# erase partition table completely
+sectors=`blockdev --getsize $DISKPART_DEV`
+tput setaf 2; echo "$DISKPART_DEV has $sectors sectors in total, erase in 10 sec..."; tput sgr0;
+sleep 10
 
-	tput setaf 2; echo 'creating partition table...'; tput sgr0;
-	# GPT table creation
-	parted $DISKPART_DEV --script mklabel gpt
-	# 2M grub loader
-	parted $DISKPART_DEV --script mkpart primary 1MiB 3MiB
-	parted $DISKPART_DEV --script name $N_BIOSBOOT tkarch_grub
-	parted $DISKPART_DEV --script set $N_BIOSBOOT bios_grub on
-	# 1GiB boot partition
-	parted $DISKPART_DEV --script mkpart primary 4MiB 1000MiB
-	# 4GiB swap partition
-	parted $DISKPART_DEV --script mkpart primary 1001MiB 5000MiB
-	# root partition using the rest of the disk
-	parted $DISKPART_DEV --script mkpart primary 5001MiB -- -1MiB
-	# mark EFI System Partition
-	parted $DISKPART_DEV --script set $N_BOOT boot on
-fi;
+tput setaf 2; echo "erase disk head ..."; tput sgr0;
+dd if=/dev/zero of=$DISKPART_DEV bs=512 count=8192
+tput setaf 2; echo "erase disk tail ..."; tput sgr0;
+dd if=/dev/zero of=$DISKPART_DEV bs=512 seek=`expr $sectors - 8192` count=8192
 
-tput setaf 2; echo 'name partitions...'; tput sgr0;
+tput setaf 2; echo 'creating partitions and make filesystems...'; tput sgr0;
+# GPT table creation
+parted $DISKPART_DEV --script mklabel gpt
+# 2M grub boot code
+parted $DISKPART_DEV --script mkpart primary 1MiB 3MiB
+parted $DISKPART_DEV --script name $N_BIOSBOOT tkarch_grub
+parted $DISKPART_DEV --script set $N_BIOSBOOT bios_grub on
+# 1GiB boot partition
+parted $DISKPART_DEV --script mkpart primary 4MiB 1000MiB
 parted $DISKPART_DEV --script name $N_BOOT tkarch_boot
-parted $DISKPART_DEV --script name $N_SWAP tkarch_swap
-parted $DISKPART_DEV --script name $N_ROOT tkarch_root
-
-tput setaf 2; echo 'making filesystem...'; tput sgr0;
-if ! $GRUB_ELF_INSTALL; then mkfs.vfat $DISKPART_BIOSBOOT; fi;
+parted $DISKPART_DEV --script set $N_BOOT boot on
 mkfs.ext4 -F $DISKPART_BOOT
-mkswap -f    $DISKPART_SWAP
-swapon       $DISKPART_SWAP
+# 4GiB swap partition
+parted $DISKPART_DEV --script mkpart primary 1001MiB 5000MiB
+parted $DISKPART_DEV --script name $N_SWAP tkarch_swap
+mkswap -f $DISKPART_SWAP
+swapon    $DISKPART_SWAP
+# root partition using the rest of the disk
+parted $DISKPART_DEV --script mkpart primary 5001MiB -- -1MiB
+parted $DISKPART_DEV --script name $N_ROOT tkarch_root
 mkfs.ext4 -F $DISKPART_ROOT
 
 tput setaf 2; echo 'resulting parition:'; tput sgr0;
--- setup/grub_install/setup.sh
+++ setup/grub_install/setup.sh
@@ -1,14 +1,17 @@
 if $GRUB_ELF_INSTALL; then
-	pacmanS efibootmgr
-	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+    pacmanS efibootmgr
 
-	# change EFI entry icon
-	cp /root/tkarch/setup/grub_install/os_linux.icns /boot/efi/EFI/arch_grub/grubx64.icns
+    grub-install --target=x86_64-efi \
+        --boot-directory="/boot" --efi-directory="/boot" \
+        --bootloader-id=arch_grub --recheck
 
-	# list what we got
-	find /boot/efi/
+    # change EFI entry icon
+    cp /root/tkarch/setup/grub_install/os_linux.icns /boot/efi/EFI/arch_grub/grubx64.icns
+
+    # list what we got
+    find /boot/efi/
 else
-	grub-install --recheck $DISKPART_DEV
+    grub-install --recheck $DISKPART_DEV
 fi;
 
 grub-mkconfig -o /boot/grub/grub.cfg
