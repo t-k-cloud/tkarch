f2d5b3cb98dc179301be4989683688e74d5b54e9
w32zhong <clock126@126.com>
2026-03-22 12:25:25 -0400

    fixup global_scripts/ai-local-sgl.sh

--- global_scripts/ai-local-sgl.sh
+++ global_scripts/ai-local-sgl.sh
@@ -1,1 +1,9 @@
-docker compose -f ai-local-sgl.yml up
+#!/bin/bash
+
+SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+SCRIPT_NAME=$(basename "$SCRIPT_PATH")
+
+YML_FILE="${SCRIPT_NAME%.sh}.yml"
+set -x
+docker compose -f "${SCRIPT_DIR}/${YML_FILE}" up
